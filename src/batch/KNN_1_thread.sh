2e7b4af3ae0991f4b9ab6e1f9a6e15fcace071a6
Michae1 <333mmcc@gmail.com>
2020-10-08 11:02:20 +1000

    added knn batch

---
+++ src/batch/KNN_1_thread.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=KNN_demo
+#SBATCH --output=/home/s4430291/STAT4402_tut/KNN_demo.out
+#SBATCH --error=/home/s4430291/STAT4402_tut/KNN_demo.err
+#SBATCH --time=0-0:05
+#SBATCH --mem=1GB
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+
+export OMP_NUM_THREADS=1
+
+DATE=$(date +"%Y%m%d%H%M")
+echo "time started  "$DATE
+echo "This is job '$SLURM_JOB_NAME' (id: $SLURM_JOB_ID) running on the following nodes:"
+echo $SLURM_NODELIST
+echo "running with SLURM_TASKS_PER_NODE= $SLURM_TASKS_PER_NODE "
+echo "running with SLURM_NTASKS total  = $SLURM_NTASKS "
+export TIMEFORMAT="%E sec"
+
+python3 /home/s4430291/STAT4402_tut/KNN_demo.py
+
+DATE=$(date +"%Y%m%d%H%M")
+echo "time finished "$DATE
